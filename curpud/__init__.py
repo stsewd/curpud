8bdb7b82e56f47ff624ef76c6b0b53e6ff2551f3
Santos Gallegos <santos_g@outlook.com>
2017-02-05 02:51:05 -0500

    Create administration views

--- curpud/__init__.py
+++ curpud/__init__.py
@@ -1,11 +1,58 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    url_for,
+    g
+)
+from flask_admin import Admin
+from flask_admin.contrib.peewee import ModelView
+import peewee as orm
 
 
+# App global configuration
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 
+
+# jinja template configuration
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
+
+
+# Database global configuration
+db = orm.MySQLDatabase(
+    'curpud',
+    host=app.config['DB_HOST'],
+    user=app.config['DB_USER'],
+    passwd=app.config['DB_PASSWD']
+)
+
+from .publications.models import Relevance, DataBase, Jounal
+
+try:
+    db.connect()
+    db.create_tables([Relevance, DataBase, Jounal])
+except Exception as e:
+    pass
+
+
+admin = Admin(app, name='curpud', template_mode='bootstrap3')
+admin.add_view(ModelView(Relevance))
+admin.add_view(ModelView(DataBase))
+admin.add_view(ModelView(Jounal))
+
+
+@app.before_request
+def before_request():
+    g.db = db
+    g.db.connect()
+
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
 
 
 @app.errorhandler(404)
