87e5c8a9aaaffc1f0f4961b0ea50458b9b79d432
Santos Gallegos <santos_g@outlook.com>
2017-02-04 22:10:19 -0500

    Expose blueprints
    Signed-off-by: Santos Gallegos <santos_g@outlook.com>

--- curpud/__init__.py
+++ curpud/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 
 app = Flask(__name__)
@@ -7,4 +7,19 @@ app.config.from_object('config.DevelopmentConfig')
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
-from . import views
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('publications.index'))
+
+
+from .auth.views import auth
+from .publications.views import pub
+
+app.register_blueprint(auth)
+app.register_blueprint(pub)
