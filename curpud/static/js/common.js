ca88704adb4d31bdf2cf946ebc5f7c785329ad5f
Santos Gallegos <santos_g@outlook.com>
2017-02-05 00:45:45 -0500

    Add static content

---
+++ curpud/static/js/common.js
@@ -0,0 +1,60 @@
+/*jshint esversion: 6 */
+
+function createAlert(type, msg) {
+  var icon = type === 'alert-success' ? 'glyphicon-ok' : 'glyphicon-warning-sign';
+  var alertBootstrap = $('<div>', {
+    'class': `alert ${type} fade in alert-dismissible`,
+    'role': 'alert',
+    'hidden': 'hidden',
+    'html': `
+      <button type="button" class="close" data-dismiss="alert">
+      <span>&times;</span>
+      </button>
+      <span class="alert-message">
+      <span class="glyphicon ${icon} text-muted"></span> ${msg}
+    </span>`
+  });
+  $('#container-alert').append(alertBootstrap);
+  return alertBootstrap;
+}
+
+function showAlert(type, msg) {
+  var alertBootstrap = createAlert(type, msg);
+  alertBootstrap.show();
+  alertBootstrap.delay(3500).slideUp(300, function () {
+    $(this).alert('close');
+  });
+}
+
+function sendRequest(form, method, url, success) {
+  $(form).submit(function (e) {
+    $.ajax({
+      'type': method,
+      'url': url,
+      'data': $(this).serialize(),
+      'success': function (data, status) {
+        if (data.status && data.status === 'error') {
+          showAlert('alert-danger', data.description);
+        } else {
+          success(data, status);
+        }
+      },
+      'error': function (data) {
+        showAlert('alert-danger', 'Ups, algo inesperado ocurrió.');
+      }
+    });
+    e.preventDefault();
+  });
+}
+
+$(document).ready(function () {
+  $(document).pjax('a', '#main-content');
+});
+
+$(document).on('pjax:send', function() {
+  $('.loader-container').show();
+});
+
+$(document).on('pjax:complete', function() {
+  $('.loader-container').hide();
+});
