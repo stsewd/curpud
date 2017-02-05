14c1f400ad20a7816a2a5b9db6638c51fa79aff2
Santos Gallegos <santos_g@outlook.com>
2017-02-05 14:44:21 -0500

    Add tranlation

--- curpud/static/js/admin.js
+++ curpud/static/js/admin.js
@@ -3,3 +3,6 @@ var navb = $('#main-content > div > .navbar');
 
 container.removeClass('container');
 navb.remove();
+
+// Translations
+$('tbody > tr > td[colspan="999"] > div').text('No se encontraron elementos.');
