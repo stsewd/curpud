b56cc9b01069509e4ea9034977242800856b0e98
Santos Gallegos <santos_g@outlook.com>
2017-02-07 06:27:05 -0500

    Add regional journals to db

--- manage.py
+++ manage.py
@@ -19,6 +19,8 @@ def users():
     create_courses_types()
     create_admin_user()
     create_normal_user()
+    populate_databases()
+    populate_journals_regional()
 
 
 def create_relevances():
@@ -60,6 +62,47 @@ def create_normal_user():
         passwd='1145cbf42070c6704b66d6ac75347726'  # profe
     )
     user.save()
+
+
+def populate_databases():
+    from curpud.publications.models import DataBase, Relevance
+    DataBase.create(
+        name='ebsco',
+        relevance=Relevance.get(Relevance.name == 'regional')
+    )
+    DataBase.create(
+        name='esmerald',
+        relevance=Relevance.get(Relevance.name == 'regional')
+    )
+    DataBase.create(
+        name='web of knowledge',
+        relevance=Relevance.get(Relevance.name == 'internacional')
+    )
+    DataBase.create(
+        name='scopus',
+        relevance=Relevance.get(Relevance.name == 'internacional')
+    )
+
+
+def populate_journals_regional():
+    import csv
+    from curpud.publications.models import Journal, DataBase
+    base_path = 'scripts/output/'
+    for filename, database in [('ebsco.csv', 'ebsco'), ('emerald.csv', 'esmerald')]:
+        with open(base_path + filename) as csvfile:
+            csv_reader = csv.reader(csvfile)
+            next(csv_reader)  # Headers
+            for row in csv_reader:
+                short_name, long_name, issn = row
+                try:
+                    Journal.create(
+                        name=long_name,
+                        short_name=short_name,
+                        issn=issn,
+                        database=DataBase.get(DataBase.name == database)
+                    )
+                except:
+                    continue
 
 
 if __name__ == "__main__":
